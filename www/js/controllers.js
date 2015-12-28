65750cba48562d57dc2c6021056c84cdefdced14
Swapnil Patwa <patwaswapnil@gmail.com>
2015-12-29 01:36:01 +0530

    login module

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,80 +1,95 @@
-angular.module('starter.controllers', [])
+var app = angular.module('foodex.controllers', [])
 
-.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
+app.controller('AppCtrl', function($scope, $ionicModal, $timeout, $ionicPopup) {
 
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
+    $scope.showLogin = true;
+    $scope.directClose = false; //if register button direct clicked
+    $scope.registerToggle = function() {
+        if ($scope.directClose == true) {
+            $scope.modal.hide();
+            $scope.showLogin = !$scope.showLogin;
+        } else {
+            $scope.showLogin = !$scope.showLogin;
+        }
+    }
+    $scope.registerOpen = function() {
+        $scope.showLogin = !$scope.showLogin;
+        $scope.directClose = false;
+    }
+    $ionicModal.fromTemplateUrl('templates/login.html', {
+        scope: $scope
+    }).then(function(modal) {
+        $scope.modal = modal;
+    });
+    $scope.closeLogin = function() {
+        $scope.modal.hide();
+    };
+    $scope.login = function() {
+        $scope.modal.show();
+    };
+    $scope.registerShow = function() {
+        $scope.showLogin = false;
+        $scope.directClose = true;
 
-  // Form data for the login modal
-  $scope.loginData = {};
+        $scope.modal.show();
+    };
 
-  // Create the login modal that we will use later
-  $ionicModal.fromTemplateUrl('templates/login.html', {
-    scope: $scope
-  }).then(function(modal) {
-    $scope.modal = modal;
-  });
+    $scope.resetPwd = function() {
+        $scope.data = {}
 
-  // Triggered in the login modal to close it
-  $scope.closeLogin = function() {
-    $scope.modal.hide();
-  };
+        // An elaborate, custom popup
+        var myPopup = $ionicPopup.show({
+            template: '<input type="email" ng-model="data.email" placeholder="Enter you email" class="padding">',
+            title: 'Enter your email address',
+            subTitle: 'You will get a link to reset password',
+            scope: $scope,
+            buttons: [{
+                text: 'Cancel',
+                type: 'fs12'
+            }, {
+                text: 'Submit',
+                type: 'button-balanced fs12',
+                onTap: function(e) {
+                    if (!$scope.data.email) {
+                        //don't allow the user to close unless he enters wifi password
+                        e.preventDefault();
+                    } else {
+                        return $scope.data.email;
+                    }
+                }
+            }, ]
+        });
+        myPopup.then(function(res) {
+            console.log('Tapped!', res);
+        });
 
-  // Open the login modal
-  $scope.login = function() {
-    $scope.modal.show();
-  };
+    };
 
-  // Perform the login action when the user submits the login form
-  $scope.doLogin = function() {
-    console.log('Doing login', $scope.loginData);
+});
+app.controller('LocationCtrl', function($scope ) {
+     
 
-    // Simulate a login delay. Remove this and replace with your login
-    // code if using a login system
-    $timeout(function() {
-      $scope.closeLogin();
-    }, 1000);
-  };
-})
+    $scope.model = "";
 
-.controller('PlaylistsCtrl', function($scope) {
-  $scope.playlists = [
-    { title: 'Reggae', id: 1 },
-    { title: 'Chill', id: 2 },
-    { title: 'Dubstep', id: 3 },
-    { title: 'Indie', id: 4 },
-    { title: 'Rap', id: 5 },
-    { title: 'Cowbell', id: 6 },
-    { title: 'Reggae', id: 7 },
-    { title: 'Chill', id: 8 },
-    { title: 'Dubstep', id: 9 },
-    { title: 'Indie', id: 10 },
-    { title: 'Rap', id: 11 },
-    { title: 'Cowbell', id: 12 },
-    { title: 'Reggae', id: 13 },
-    { title: 'Chill', id: 14 },
-    { title: 'Dubstep', id: 15 },
-    { title: 'Indie', id: 16 },
-    { title: 'Rap', id: 17 },
-    { title: 'Cowbell', id: 18 },
-    { title: 'Reggae', id: 19 },
-    { title: 'Chill', id: 20 },
-    { title: 'Dubstep', id: 21 },
-    { title: 'Indie', id: 22},
-    { title: 'Rap', id: 23 },
-    { title: 'Cowbell', id: 24 },
-    { title: 'Reggae', id: 25 },
-    { title: 'Chill', id: 26 },
-    { title: 'Dubstep', id: 27 },
-    { title: 'Indie', id: 28 },
-    { title: 'Rap', id: 29 },
-    { title: 'Cowbell', id: 30 }
-  ];
-})
+    $scope.getTestItems = function(query) {
+        if (query) {
+            return {
+                items: [
 
-.controller('PlaylistCtrl', function($scope, $stateParams) {
+                    {
+                        id: "3",
+                        name: query + "3",
+                        view: "Location: " + query + "3"
+                    }
+                ]
+            };
+        }
+        return {
+            items: []
+        };
+    }
+
+});
+app.controller('HomeCtrl', function($scope ) {
+ 
 });
